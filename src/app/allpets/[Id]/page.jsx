dfb98aa6d27cb019d7ebbe9b23d0afd32f9da487
Navied-dipu <navieddipu08@gmail.com>
2026-05-21 19:35:19 +0600

    vercel

--- src/app/allpets/[Id]/page.jsx
+++ src/app/allpets/[Id]/page.jsx
@@ -23,7 +23,7 @@ import {
 
 const PetDetailsPage = ({ params }) => {
   const router = useRouter();
-  const { id } = React.use(params);
+  const { id } = params;
 
   const { data: session, isPending } = authClient.useSession();
   const user = session?.user;
